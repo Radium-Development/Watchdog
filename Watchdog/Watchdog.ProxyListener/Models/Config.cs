26fab3b47051fbf35cbd1b08a182d64809be1165
tsukictf <tsukictf@gmail.com>
2021-08-19 16:59:57 -0400

    build test

--- Watchdog/Watchdog.ProxyListener/Models/Config.cs
+++ Watchdog/Watchdog.ProxyListener/Models/Config.cs
@@ -10,6 +10,6 @@ namespace Watchdog.ProxyListener.Models
     {
         public ushort WatchdogListenerPort { get; set; } = 3000; // Port to listen for traffic on
         public ushort WatchdogOutgoingPort { get; set; } = 3001; // Port to send data too once processed
-        public string[] ListnerIPs { get; set; } = [ "127.0.0.1", "0.0.0.0" ];
+        public string[] ListnerIPs { get; set; } = { "127.0.0.1", "0.0.0.0" };
     }
 }
