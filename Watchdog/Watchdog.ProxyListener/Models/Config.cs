f9eaab8276ae56601a798c8a9e8557b49af9de48
tsukictf <tsukictf@gmail.com>
2021-08-19 20:46:44 -0400

    Added mongodb connection class

--- Watchdog/Watchdog.ProxyListener/Models/Config.cs
+++ Watchdog/Watchdog.ProxyListener/Models/Config.cs
@@ -10,10 +10,9 @@ namespace Watchdog.ProxyListener.Models
     {
         public ushort WatchdogListenerPort { get; set; } = 3000; // Port to listen for traffic on
         public ushort WatchdogOutgoingPort { get; set; } = 3001; // Port to send data too once processed
-<<<<<<< HEAD
         public string[] ListnerIPs { get; set; } = { "127.0.0.1", "localhost" };
-=======
-        public string[] ListnerIPs { get; set; } = { "127.0.0.1", "0.0.0.0" };
->>>>>>> 26fab3b47051fbf35cbd1b08a182d64809be1165
+        public string RemoteDBConnectionString { get; set; } = "mongodb://Watchdog:Watchdog@10.0.0.171:27017/WatchdogSuspects?authSource=WatchdogSuspects";
+        public string RemoteDBDatabaseName { get; set; } = "WatchdogSuspects";
+        public string RemoteDBCollectionName { get; set; } = "strings";
     }
 }
