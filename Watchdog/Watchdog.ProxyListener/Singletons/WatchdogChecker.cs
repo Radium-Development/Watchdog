293023e3646f9e95a41a538fb575623a715fc8b1
tsukictf <tsukictf@gmail.com>
2021-08-28 19:40:42 -0400

    Use dictionary instead of regex list to record type of attack string

--- Watchdog/Watchdog.ProxyListener/Singletons/WatchdogChecker.cs
+++ Watchdog/Watchdog.ProxyListener/Singletons/WatchdogChecker.cs
@@ -9,12 +9,10 @@ namespace Watchdog.ProxyListener.Singletons
     public class WatchdogChecker
     {
         private Config _config { get; }
-
         private ILogger _logger { get; }
-
-        private IRemoteDB _database { get;  }
-
-        private List<Regex> _attackStrRegexList { get; }
+        private IRemoteDB _database { get; }
+        // (key, value) pair dictionary of (attack string text, type of attack) 
+        private Dictionary<Regex, string> _attackStrDict { get; }
 
         public WatchdogChecker(Config config, ILogger logger, IRemoteDB database)
         {
@@ -22,11 +20,11 @@ namespace Watchdog.ProxyListener.Singletons
             this._logger = logger;
             this._database = database;
             // fetch documents from remote database and save locally to save bandwidth
-            this._attackStrRegexList = new List<Regex>();
+            this._attackStrDict = new Dictionary<Regex, string>();
             foreach (AttackString attackStr in _database.GetAllAttackStrings())
             {
                 Regex rx = new Regex(@attackStr.String, RegexOptions.IgnoreCase);
-                this._attackStrRegexList.Add(rx);
+                this._attackStrDict.Add(rx, attackStr.Type);
             }
         }
 
@@ -35,12 +33,13 @@ namespace Watchdog.ProxyListener.Singletons
             bool contains = false;
 
             // see if data contains for every attack string
-            foreach (Regex rx in _attackStrRegexList)
+            foreach (Regex rx in _attackStrDict.Keys)
             {
                 if (rx.IsMatch(data))
                 {
                     contains = true;
-                    _logger.Log($"Attack string REGEX match found: {rx.ToString()}", LogSeverity.INFO);
+                    string type = _attackStrDict[rx];
+                    _logger.Log($"[{type}] string found: {rx.ToString()}", LogSeverity.INFO);
                     break;  // only need to find once
                 }
             }
